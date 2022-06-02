be7929ffb266bd96e5ac5b249581c82550dc594d
aizaq1 <qaziashikin@gmail.com>
2022-06-02 18:46:21 -0400

    setup of new page and styling

---
+++ values.js
@@ -0,0 +1,4 @@
+// const resultsList = document.getElementById("results");
+// const searcher = new URLSearchParams(window.location.search);
+
+// // console.log(searcher.getElementById("m1rows").value);
