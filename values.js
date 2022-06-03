61264006ae7e75d3530639607c6722176a5abf9c
aizaq1 <qaziashikin@gmail.com>
2022-06-03 14:45:58 -0400

    cleaned up code

--- values.js
+++
@@ -1,4 +0,0 @@
-// const resultsList = document.getElementById("results");
-// const searcher = new URLSearchParams(window.location.search);
-
-// // console.log(searcher.getElementById("m1rows").value);
