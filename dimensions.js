c144d2c4aba7d1fe217e87b9f99d3351cd2550ed
aizaq1 <qaziashikin@gmail.com>
2022-06-02 20:47:14 -0400

    added functionality of producing matrices

--- dimensions.js
+++ dimensions.js
@@ -9,10 +9,14 @@ const m2rows = document.getElementById("m2rows");
 const m2cols = document.getElementById("m2cols");
 const myForm = document.querySelector("#rowColCountForm");
 
+const valueBoxesArea = document.querySelector(".valueBoxes");
+
 myForm.addEventListener("submit", checkValid);
 
 function checkValid(err){
+    valueBoxesArea.innerHTML = "";
     err.preventDefault();
+    
 
     if (m1cols.value != m2rows.value){
         m1colsError.classList.add("error");
@@ -25,6 +29,24 @@ function checkValid(err){
             m2rowsError.remove();
         }, 5000);
     }     
-    else 
-        myForm.submit();
+    else
+        produceValueBoxes(m1rows.value, m1cols.value, m2rows.value, m2cols.value);            
+
+}
+
+function produceValueBoxes(m1rows, m1cols, m2rows, m2cols){
+    // Matrix 1
+    valueBoxesArea.innerHTML += '<p class="valueBoxTitle">Matrix 1</p>'
+    for (let i = 1; i <= m1rows; i++) {
+        for (let j = 1; j <= m1cols; j++)
+            valueBoxesArea.innerHTML += `<input type="number" class="valueBox" id="m1valueBox${i}${j}" value="0"></input>`;
+        valueBoxesArea.innerHTML += "<div></div>";
+    }
+    // Matrix 2
+    valueBoxesArea.innerHTML += '<p class="valueBoxTitle">Matrix 2</p>'
+    for (let i = 1; i <= m2rows; i++) {
+        for (let j = 1; j <= m2cols; j++)
+            valueBoxesArea.innerHTML += `<input type="number" class="valueBox" id="m2valueBox${i}${j}" value="0"></input>`;
+        valueBoxesArea.innerHTML += "<div></div>";
+    }    
 }
